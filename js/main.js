cffdf030cd7f89622480903b6e2e317e2c4787a6
Marcelo Querque <querquemarcelo@gmail.com>
2023-05-31 20:35:11 -0300

    Organize code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const scoreInfo = document.querySelector(".score-info").innerHTML;
-const scoreMessage = document.querySelector(".score-message").innerHTML;
+var scoreMessage = document.querySelector(".score-message");
 const playerChoice = document.querySelector(".player-choice").innerHTML;
 const playerScore = document.querySelector(".player-score").innerHTML;
 const compChoice = document.querySelector(".comp-choice").innerHTML;
@@ -10,7 +10,19 @@ Array.from(options).forEach((element) => {
     element.addEventListener('click', userChoice)
 })
 
+function getComputerChoice() {
+    const compChoice = Math.floor(Math.random() * 3) + 1;
+    if (compChoice === 1) {
+        document.querySelector(".comp-choice").innerHTML = "✊"
+    } else if (compChoice === 2) {
+        document.querySelector(".comp-choice").innerHTML = "✋"
+    } else {
+        document.querySelector(".comp-choice").innerHTML = "✌"
+    }
+}
+
 function userChoice(click) {
+    getComputerChoice()
     if (click.target.classList.contains('rock')) {
         document.querySelector(".player-choice").innerHTML = "✊"
     } else if (click.target.classList.contains('paper')) {
@@ -22,50 +34,40 @@ function userChoice(click) {
 
 
 // function game() {
-//     let runs = 0;
-//     let userScore = 0;
-//     let compScore = 0;
+    // let runs = 0;
+// let userScore = 0;
+// let compScore = 0;
 
-//     function getComputerChoice() {
-//         const compChoice = Math.floor(Math.random() * 3) + 1;
-//         if (compChoice === 1) {
-//             return "rock"
-//         } else if (compChoice === 2) {
-//             return "paper"
-//         } else {
-//             return "scissors"
-//         }
-//     }
 
-//     function userChoice() {
-//         let userChoice = prompt("Choose: rock, paper, or scissors")
-//         return userChoice.toLowerCase()
-//     }
+// function userChoice() {
+//     let userChoice = prompt("Choose: rock, paper, or scissors")
+//     return userChoice.toLowerCase()
+// }
 
-//     function playRound(playerSelection, computerSelection) {
-//         if (playerSelection === computerSelection) {
-//             return `It's a draw! ${playerSelection} equals ${computerSelection}`
-//         } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
-//             userScore += 1
-//             return `You win! ${playerSelection} beats ${computerSelection}`
-//         } else {
-//             compScore += 1
-//             return `You lose! ${computerSelection} beats ${playerSelection}`
-//         }
-//     }
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        scoreMessage = `It's a draw! ${playerSelection} equals ${computerSelection}`
+    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
+        userScore += 1
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        compScore += 1
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
 
-//     while (runs < 5) {
-//         playRound(userChoice(), getComputerChoice());
-//         runs += 1;
-//     }
-    
-//     if (userScore > compScore) {
-//         console.log(`You won! User score: ${userScore} | Comp. Score: ${compScore}`)
-//     } else if (compScore > userScore) {
-//         console.log(`You lost! User score: ${userScore} | Comp. Score: ${compScore}`)
-//     } else {
-//         console.log(`It's a draw! User score: ${userScore} | Comp. Score: ${compScore}`)
-//     }
+// while (runs < 5) {
+//     playRound(userChoice(), getComputerChoice());
+//     runs += 1;
+// }
+
+if (userScore > compScore) {
+    console.log(`You won! User score: ${userScore} | Comp. Score: ${compScore}`)
+} else if (compScore > userScore) {
+    console.log(`You lost! User score: ${userScore} | Comp. Score: ${compScore}`)
+} else {
+    console.log(`It's a draw! User score: ${userScore} | Comp. Score: ${compScore}`)
+}
 // }
 
 // game()
