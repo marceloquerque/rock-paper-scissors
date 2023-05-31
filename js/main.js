4d5f7e29c66ffa23d2c63416a98ea29011531ba8
Marcelo Querque <querquemarcelo@gmail.com>
2023-05-31 08:08:39 -0300

    Base CSS

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
-function game() {
-    let runs = 0;
-    let userScore = 0;
-    let compScore = 0;
+// function game() {
+//     let runs = 0;
+//     let userScore = 0;
+//     let compScore = 0;
 
-    function getComputerChoice() {
-        const compChoice = Math.floor(Math.random() * 3) + 1;
-        if (compChoice === 1) {
-            return "rock"
-        } else if (compChoice === 2) {
-            return "paper"
-        } else {
-            return "scissors"
-        }
-    }
+//     function getComputerChoice() {
+//         const compChoice = Math.floor(Math.random() * 3) + 1;
+//         if (compChoice === 1) {
+//             return "rock"
+//         } else if (compChoice === 2) {
+//             return "paper"
+//         } else {
+//             return "scissors"
+//         }
+//     }
 
-    function userChoice() {
-        let userChoice = prompt("Choose: rock, paper, or scissors")
-        return userChoice.toLowerCase()
-    }
+//     function userChoice() {
+//         let userChoice = prompt("Choose: rock, paper, or scissors")
+//         return userChoice.toLowerCase()
+//     }
 
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-            return `It's a draw! ${playerSelection} equals ${computerSelection}`
-        } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
-            userScore += 1
-            return `You win! ${playerSelection} beats ${computerSelection}`
-        } else {
-            compScore += 1
-            return `You lose! ${computerSelection} beats ${playerSelection}`
-        }
-    }
+//     function playRound(playerSelection, computerSelection) {
+//         if (playerSelection === computerSelection) {
+//             return `It's a draw! ${playerSelection} equals ${computerSelection}`
+//         } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
+//             userScore += 1
+//             return `You win! ${playerSelection} beats ${computerSelection}`
+//         } else {
+//             compScore += 1
+//             return `You lose! ${computerSelection} beats ${playerSelection}`
+//         }
+//     }
 
-    while (runs < 5) {
-        playRound(userChoice(), getComputerChoice());
-        runs += 1;
-    }
+//     while (runs < 5) {
+//         playRound(userChoice(), getComputerChoice());
+//         runs += 1;
+//     }
     
-    if (userScore > compScore) {
-        console.log(`You won! User score: ${userScore} | Comp. Score: ${compScore}`)
-    } else if (compScore > userScore) {
-        console.log(`You lost! User score: ${userScore} | Comp. Score: ${compScore}`)
-    } else {
-        console.log(`It's a draw! User score: ${userScore} | Comp. Score: ${compScore}`)
-    }
-}
+//     if (userScore > compScore) {
+//         console.log(`You won! User score: ${userScore} | Comp. Score: ${compScore}`)
+//     } else if (compScore > userScore) {
+//         console.log(`You lost! User score: ${userScore} | Comp. Score: ${compScore}`)
+//     } else {
+//         console.log(`It's a draw! User score: ${userScore} | Comp. Score: ${compScore}`)
+//     }
+// }
 
-game()
+// game()
