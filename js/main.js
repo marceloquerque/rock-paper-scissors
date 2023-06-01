4b4c60c13ec5effdca3010b7b13375404a5eca45
Marcelo Querque <querquemarcelo@gmail.com>
2023-06-01 08:50:36 -0300

    Getting there

--- js/main.js
+++ js/main.js
@@ -4,30 +4,77 @@ Array.from(options).forEach(element => {
     element.addEventListener("click", playGame)
 })
 
-function playGame(click) {
-    let showPlayerChoice = document.querySelector(".player-choice");
+let showPlayerChoice = document.querySelector(".player-choice");
+let showCompChoice = document.querySelector(".comp-choice");
+let scoreInfo = document.querySelector(".score-info");
+let scoreMessage = document.querySelector(".score-message");
+let playerScoreElement = document.querySelector(".player-score");
+let compScoreElement = document.querySelector(".comp-score");
+let playerScore = 0;
+let compScore = 0;
 
+document.querySelector(".restart").addEventListener("click", restartGame)
+
+function restartGame() {
+    playerScore = 0;
+    compScore = 0;
+    scoreInfo.innerHTML = "Choose your weapon"
+    scoreMessage.innerHTML = "First to score 5 points wins the game"
+    showPlayerChoice.innerHTML = "❔"
+    showCompChoice.innerHTML = "❔"
+    playerScoreElement.innerHTML = "0"
+    compScoreElement.innerHTML = "0"
+}
+
+function playGame(click) {
+    const playerChoice = click.target.id;
+    
     function randomChoice() {
-        return Math.floor(Math.random() * 3) + 1
+        let choice = Math.floor(Math.random() * 3) + 1
+        if (choice === 1) {
+            showCompChoice.innerHTML = "✊"
+            return "rock"
+        } else if (choice === 2) {
+            showCompChoice.innerHTML = "✋"
+            return "paper"
+        } else if (choice === 3) {
+            showCompChoice.innerHTML = "✌"
+            return "scissors"
+        }
     }
 
     const compChoice = randomChoice()
 
-    const getClick = click.target.id;
-    function returnPlayerChoice(getClick) {
-        if (getClick === "rock") {
+    function returnPlayerChoice() {
+        if (playerChoice === "rock") {
             showPlayerChoice.innerHTML = "✊"
-            return 1
-        } else if (getClick === "paper") {
+        } else if (playerChoice === "paper") {
             showPlayerChoice.innerHTML = "✋"
-            return 2
-        } else if (getClick === "scissors"){
+        } else if (playerChoice === "scissors"){
             showPlayerChoice.innerHTML = "✌"
-            return 3
         }
     }  
-    
-    const playerChoice = returnPlayerChoice(getClick)
+
+    returnPlayerChoice()
+
+    const roundWinner = (playerChoice, compChoice) => {
+        if (playerChoice === compChoice) {
+            scoreInfo.innerHTML = "It's a draw!"
+            scoreMessage.innerHTML = `Both chose ${playerChoice}`
+        } else if (playerChoice === "rock" && compChoice === "scissors" || playerChoice === "paper" && compChoice === "rock" || playerChoice === "scissors" && compChoice === "paper") {
+            scoreInfo.innerHTML = "You win!"
+            scoreMessage.innerHTML = `${playerChoice} beats ${compChoice}`
+            playerScore += 1
+            playerScoreElement.innerHTML = playerScore
+        } else {
+            scoreInfo.innerHTML = "You lose!"
+            scoreMessage.innerHTML = `${compChoice} beats ${playerChoice}`
+            compScore += 1
+            compScoreElement.innerHTML = compScore
+        }
+    }
+
+    roundWinner(playerChoice, compChoice)
 
     console.log(playerChoice)
     console.log(compChoice)
